b7391d479cdcf1153c399b3933dfd1602ed4cc97
OsnovaDT <maestrojane@gmail.com>
2022-02-05 21:18:51 +0300

    Add pagination for VPS viewset

--- api/views.py
+++ api/views.py
@@ -1,9 +1,17 @@
 """Views for api app"""
 
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from api.models import VPS
 from api.serializers import VPSSerializer
+
+
+class VPSPagination(PageNumberPagination):
+    """Pagination for VPS viewset"""
+
+    page_size = 20
+    page_size_query_param = 'page_size'
 
 
 class VPSViewSet(viewsets.ModelViewSet):
@@ -11,4 +19,5 @@ class VPSViewSet(viewsets.ModelViewSet):
 
     queryset = VPS.objects.all()
 
+    pagination_class = VPSPagination
     serializer_class = VPSSerializer
